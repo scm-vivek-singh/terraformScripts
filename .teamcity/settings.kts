46e4f8c1830865e32d98a9ba4f2a96f1d0492f13
vivek <vyksingh@yahoo.com>
2022-04-15 13:37:34 +0530

    kt code update

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 
@@ -6,16 +7,50 @@ version = "2021.2"
 
 project {
 
-    buildType(Build)
+    buildType(INIT)
+    buildType(PLAN)
+    buildType(APPLY)
+
+    sequential {
+        buildType(INIT)
+        buildType(PLAN)
+        buildType(APPLY)
+    }
 }
 
-object Build : BuildType({
-    name = "Build"
+object INIT : BuildType({
+    name = "terraform init"
 
     vcs {
         root(DslContext.settingsRoot)
     }
 
+    steps {
+        script { """terraform init""" }
+    }
+
+})
+
+object PLAN : BuildType({
+    name = "plan"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+    steps {
+        script { """terraform plan""" }
+    }
+})
+
+object APPLY : BuildType({
+    name = "terraform apply"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+    steps {
+        script { """terraform apply""" }
+    }
     triggers {
         vcs {
         }
