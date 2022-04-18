2a16b65b3a2844e888d4843e1b458dcfd186a3ec
vivek <vyksingh@yahoo.com>
2022-04-18 10:18:41 +0530

    kt code update

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,44 +5,50 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.2"
 
+fun com() {
+    print("Enter text: ")
+    val stringInput = readLine()
+    println("You entered: $stringInput")
+}
+
 project {
 
-    buildType(INIT)
-    buildType(PLAN)
+//    buildType(INIT)
+//    buildType(PLAN)
     buildType(APPLY)
 
     sequential {
-        buildType(INIT)
-        buildType(PLAN)
+//        buildType(INIT)
+//        buildType(PLAN)
         buildType(APPLY)
     }
 }
 
-object INIT : BuildType({
-    name = "terraform init"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            scriptContent ="terraform init" }
-    }
-
-})
-
-object PLAN : BuildType({
-    name = "plan"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-    steps {
-        script {
-            scriptContent ="terraform plan" }
-    }
-})
+//object INIT : BuildType({
+//    name = "terraform init"
+//
+//    vcs {
+//        root(DslContext.settingsRoot)
+//    }
+//
+//    steps {
+//        script {
+//            scriptContent ="terraform init" }
+//    }
+//
+//})
+//
+//object PLAN : BuildType({
+//    name = "plan"
+//
+//    vcs {
+//        root(DslContext.settingsRoot)
+//    }
+//    steps {
+//        script {
+//            scriptContent ="terraform plan" }
+//    }
+//})
 
 object APPLY : BuildType({
     name = "terraform apply"
@@ -50,12 +56,15 @@ object APPLY : BuildType({
     vcs {
         root(DslContext.settingsRoot)
     }
+    com()
     steps {
         script {
-            scriptContent ="terraform apply" }
+            scriptContent="dir" }
     }
     triggers {
         vcs {
         }
     }
 })
+
+
