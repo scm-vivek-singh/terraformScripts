4aca0b43f77dd1cad72107046687aefb140d7d21
scm-vivek-singh <85884400+scm-vivek-singh@users.noreply.github.com>
2022-05-07 14:27:09 +0530

    Update pythonX.py

--- pythonX.py
+++ pythonX.py
@@ -1,1 +1,7 @@
 print('Hello World!!!')
+print("Would you like to proceed with changes.")
+response = input("select yes/no to proceed or discard respectively: ")
+if response == "yes":
+  print("Changes were applied!!!!!!!")
+else:
+  print("Exit from code!!!!!!!!!!")  
