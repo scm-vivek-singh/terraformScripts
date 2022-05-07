b6e325728fae296e6893b91a6c27a36f033980b9
scm-vivek-singh <85884400+scm-vivek-singh@users.noreply.github.com>
2022-05-07 15:21:30 +0530

    Update pythonX.py

--- pythonX.py
+++ pythonX.py
@@ -3,7 +3,7 @@ project = os.environ['project']
 print('Hello World!!! to ', project)
 
 print("Would you like to proceed with changes.")
-response = input("select yes/no to proceed or discard respectively: ")
+response = os.environ['approval']
 if response == "yes":
   print("Changes were applied!!!!!!!")
 else:
