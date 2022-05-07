473792e9426ae8e796812f81ca67a7ec76b6f2fe
scm-vivek-singh <85884400+scm-vivek-singh@users.noreply.github.com>
2022-05-07 15:36:52 +0530

    Update pythonX.py

--- pythonX.py
+++ pythonX.py
@@ -1,11 +1,11 @@
-import os
+import os, time
 project = os.environ['project']
 print('Hello World!!! to ', project)
 
-print("DO I have your approval to proceed")
-
-while os.environ['approval'] != None:
-  if os.environ['approval'] == "yes":
-    print("Changes were applied!!!!!!!")
-  else:
-    print("Exit from code!!!!!!!!!!")  
+print("DO I have your approval to proceed?")
+time.sleep(60)
+approval = input("enter yes/no")
+if approval == "yes":
+  print("Changes were applied!!!!!!!")
+else:
+  print("Exit from code!!!!!!!!!!")  
